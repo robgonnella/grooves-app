ebe45eb39ce4bf368b45256809355ec58ea14cda
Rob Gonnella <robgonnella@gmail.com>
2015-12-17 14:04:25 -0800

    add and conncect ui  templates

--- client/js/app/grooves.config.js
+++ client/js/app/grooves.config.js
@@ -5,18 +5,34 @@
     .module("grooves-app")
     .config(configure);
 
-  configure.$inject = ["$httpProvider", "$stateProvider"];
+  configure.$inject = ["$httpProvider", "$stateProvider", "$urlRouterProvider"];
 
-  function configure($httpProvider, $stateProvider) {
+  function configure($httpProvider, $stateProvider, $urlRouterProvider) {
     // console.log("Adding tokenSigningService interceptor.");
     $httpProvider.interceptors.push("tokenSigningService");
 
     $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'templates/allrecords.html'
+      })
 
       .state('account', {
         url: '/user/records',
         templateUrl: 'templates/userrecords.html'
+      })
+
+      .state('record', {
+        url: '/record',
+        templateUrl: 'templates/record.html'
+      })
+
+      .state('cart', {
+        url: '/cart',
+        templateUrl: 'templates/cart.html'
       });
+
+    $urlRouterProvider.otherwise("/");
   }
 
 
