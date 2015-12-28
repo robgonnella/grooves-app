965e0253427ea8cae334e13f84090b510b6d1920
Rob Gonnella <robgonnella@gmail.com>
2015-12-28 14:29:11 -0800

    finish cart functionality, items hidden when added from main page, items returned when removed from cart, cart view updated immediately when item removed, cart total cost and number of items updated each time item is added or removed

--- public/js/app/cart.controller.js
+++ public/js/app/cart.controller.js
@@ -10,13 +10,15 @@
   function CartController ($log, cartDataService) {
     var vm = this;
     vm.cart = cartDataService;
-    vm.list = vm.cart.list;
+    // vm.list = vm.cart.list;
     vm.remove = vm.cart.remove;
-    vm.total = totalCost();
+    vm.total = totalCost;
+
+    totalCost();
 
     function totalCost () {
       var total = 0
-      vm.list.forEach(function(item) {
+      vm.cart.list.forEach(function(item) {
         total = total + item.price;
       });
       return total;
