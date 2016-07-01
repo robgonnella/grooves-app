70a111e697ebb11ba861a32cd8f8e927ca133eb6
Rob Gonnella <robgonnella@gmail.com>
2016-06-30 23:42:04 -0700

    delete database scripts now working

--- helpers/delete_db.sh
+++ helpers/delete_db.sh
@@ -2,11 +2,23 @@
 
 echo "Script is now starting..."
 
-sleep 3
+sleep 2
+
+echo "Saving Current Working Directory"
+
+cwd=$pwd
+
+sleep 2
+
+echo "Changing to app directory"
+
+cd ~/code/wdi/grooves-app
+
+sleep 2
 
 echo "Saving Directory to Variable..."
 
-PATH_TO_FILE="$HOME/code/wdi/grooves-app/helpers/delete_db.js"
+PATH_TO_FILE="./helpers/delete_db.js"
 
 sleep 3
 
@@ -17,5 +29,9 @@ sleep 2
 echo "Now running heroku command to delete mongoLab database"
 
 heroku run node $PATH_TO_FILE
+
+echo "Changing back to current working directory"
+
+cd $cwd
 
 echo "process complete!"
